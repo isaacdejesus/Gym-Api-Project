b93ad0aac94fbc8b46108fbb2208882861ee3c2c
isaac <idreyes@uh.edu>
2022-07-30 17:12:59 -0500

    Updated progress

---
+++ gym-app/src/components/Exercises.tsx
@@ -0,0 +1,6 @@
+const Exercises = () => {
+    return (
+        <div>swjshn</div>
+    )
+};
+export default Exercises;
---
+++ gym-app/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+import { Box } from '@mui/material';
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SeachExercises';
+import HeroBanner from '../components/HeroBanner';
+const Home = () => {
+    return (
+        <Box>
+            <HeroBanner />
+            <SearchExercises />
+            <Exercises />
+        </Box>
+    )
+}
+
+export default Home;
